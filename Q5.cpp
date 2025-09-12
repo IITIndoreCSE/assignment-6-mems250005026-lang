12c751f55a9040c5a5f561e084cdf3915666657d
mems250005026-lang <mems250005026@iiti.ac.in>
2025-09-12 17:25:23 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,6 +1,53 @@
 // Write a program to populate a character array with letters of the alphabet at random; subsequently the letters in the array should be arranged alphabetically (without using the sort function).
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+int main() {
+    
+    const int SIZE = 10;
+    char arr[SIZE];
+
+    
+    srand(time(0));
+
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = 'a' + rand() % 26;  
+    }
+
+    
+    cout << "Random letters: [";
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i];
+        if (i < SIZE - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = 0; j < SIZE - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                char temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    
+    cout << "Sorted letters: [";
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i];
+        if (i < SIZE - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
 
 
 
