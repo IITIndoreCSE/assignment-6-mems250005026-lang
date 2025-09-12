dc8cb0c1e53a3745e827a3b481b8c66902464c24
mems250005026-lang <mems250005026@iiti.ac.in>
2025-09-12 16:51:55 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,34 +1,23 @@
-// Write a program that asks the user for an integer number and finds the sum of all natural numbers up to that number.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Test Case 1
-Input:
-Number: 5
-Output:
-Sum: 15
-Test Case 2
-Input:
-Number: 10
-Output:
-Sum: 55 */
+#include <iostream>
+using namespace std;
+int main()
+{
+  //test1
+    int n=5;
+    int sum = 0;
+    for(n;n>0;n--)
+    {
+        sum += n;
+    }
+    cout<<"sum: "<<sum<<endl;
+  //test2 
+    int n1=10;
+    int sum1 = 0;
+    for(n1;n1>0;n1--)
+    {
+        sum1 += n1;
+    }
+    cout<<"sum: "<<sum1<<endl;
+  
+  
+}
